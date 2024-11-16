f4168ea15e312fbc963fb592be86fd0d1c00c481
CarlosViniciusSantos <carlos.vinicius1@aluno.ifsp.edu.br>
2024-11-15 22:13:16 -0300

    message properties found

--- src/controllers/property/listPropertiesController.js
+++ src/controllers/property/listPropertiesController.js
@@ -5,6 +5,9 @@ const prisma = new PrismaClient()
 const listPropertiesController = async (req, res) => {
   try {
     const properties = await prisma.imoveis.findMany()
+    if (properties.length === 0) {
+      return res.status(404).json({ error: 'No properties found' })
+    }
     res.status(200).json(properties)
   } catch (error) {
     res.status(500).json({ error: 'Internal server error' })
